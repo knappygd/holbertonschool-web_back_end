38b5d32f10c83f9a66876547fe731602c05de631
knappygd <emiliodamasco04@gmail.com>
2023-08-29 16:34:06 -0700

    deleted extra loop

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,8 +7,5 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Execute multiple coroutines."""
-    finished: List[float] = []
-    list = [list.append(task_wait_random(max_delay)) for w in range(n)]
-    for task in asyncio.as_completed(list):
-        finished.append(await task)
+    finished = [list.append(task_wait_random(max_delay)) for w in range(n)]
     return finished
