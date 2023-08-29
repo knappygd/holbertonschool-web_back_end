d1674e80f57decc9ca54a0ff47cac906110cca2c
knappygd <emiliodamasco04@gmail.com>
2023-08-29 16:36:15 -0700

    fixed varname

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """Return a list of tasks."""
-import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Execute multiple coroutines."""
-    finished = [list.append(task_wait_random(max_delay)) for w in range(n)]
-    return finished
+    finished: List[float] = []
+    return [finished.append(task_wait_random(max_delay)) for w in range(n)]
