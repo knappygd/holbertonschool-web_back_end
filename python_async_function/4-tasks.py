fc3752e50dc948dcb12620d1ee617329a8fa823c
knappygd <emiliodamasco04@gmail.com>
2023-08-29 16:07:16 -0700

    added 3 and 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""."""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Execute multiple coroutines."""
+    list = []
+    finished = []
+    for created in range(n):
+        list.append(task_wait_random(max_delay))
+    for task in asyncio.as_completed(list):
+        finished.append(await task)
+    return finished
