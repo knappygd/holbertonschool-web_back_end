a9fcd4a039c165019d531ec7f5793754926f49bd
knappygd <emiliodamasco04@gmail.com>
2023-08-29 16:40:27 -0700

    separated for loop

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,5 +7,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Execute multiple coroutines."""
     finished: List[float] = []
-    tasklist = [finished.append(task_wait_random(max_delay)) for w in range(n)]
-    return sorted(tasklist)
+    for w in range(n):
+        finished.append(task_wait_random(max_delay))
+    return sorted(finished)
