cf9f8a6099649391f56a6227567bf57091ca4356
knapp <emiliodamasco04@gmail.com>
2023-08-30 06:01:32 -0700

    annotated

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Yields a random number from 1 to 10."""
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yields a random number from 1 to 10."""
     for i in range(10):
         await asyncio.sleep(1)
