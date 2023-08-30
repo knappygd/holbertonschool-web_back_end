df2f550b8a9ac87ff65b0db3299d024fd00a9375
knapp <emiliodamasco04@gmail.com>
2023-08-30 06:22:30 -0700

    fixed gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,11 +9,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measure the total runtime of parallel executed funcs."""
     start = time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
     finish = time()
     return finish - start
