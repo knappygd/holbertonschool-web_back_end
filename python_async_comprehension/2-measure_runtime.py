7155d0d960678ff7bada20bb9246031900f47f70
knapp <emiliodamasco04@gmail.com>
2023-08-30 06:19:39 -0700

    added 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Measure the total runtime of parallel executed funcs."""
+from typing import List
+from time import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the total runtime of parallel executed funcs."""
+    start = time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    finish = time()
+    return finish - start
