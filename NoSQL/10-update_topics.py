a84294c0bfd37cd0436a8a7c3dc921584af5c242
knapp <emiliodamasco04@gmail.com>
2023-09-06 07:09:08 -0700

    tried with updateMany

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,8 +5,7 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
-    mongo_collection.update(
+    mongo_collection.updateMany(
         {"name": name},
-        {"$set": {"topics": topics}},
-        {"multi": "true"}
+        {"$set": {"topics": topics}}
     )
