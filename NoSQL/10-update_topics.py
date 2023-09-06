ca7a5e73b6b6101b2bba18926c2d38e52902ea6c
knapp <emiliodamasco04@gmail.com>
2023-09-06 07:07:57 -0700

    added 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""changes all topics of a school document based on the name"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    mongo_collection.update(
+        {"name": name},
+        {"$set": {"topics": topics}},
+        {"multi": "true"}
+    )
