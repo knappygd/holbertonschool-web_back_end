8ff5aeb967a95304ae38acab1f0a2e02337966d2
knapp <emiliodamasco04@gmail.com>
2023-09-06 06:56:16 -0700

    added 8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,5 +2,10 @@
 """lists all documents in a collection"""
 import pymongo
 
+
 def list_all(mongo_collection):
-    
+    c = mongo_collection.find()
+    l = []
+    for i in c:
+        l.append(i)
+    return l
