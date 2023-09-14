86bbf7ce906a15bb87ea8c0f4991e3b522558703
knapp <emiliodamasco04@gmail.com>
2023-09-14 07:03:25 -0700

    fixed error raising and lint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,34 +1,37 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        if (typeof name !== 'string') throw TypeError("Name must be a string");
-        if (typeof length !== 'number') throw TypeError("Length must be a number");
-        if (!Array.isArray(students)) throw TypeError("Students must be an array")
-        this._name = name;
-        this._length = length;
-        this._students = students;
-    }
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError("Name must be a string");
+    if (typeof length !== 'number') throw TypeError("Length must be a number");
+    if (!Array.isArray(students)) throw TypeError("Students must be an array");
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    set name(newName) {
-        this._name = newName;
-    }
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError("Name must be a string");
+    this._name = newName;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set length(newLength) {
-        this._length = newLength;
-    }
+  set length(newLength) {
+    if (typeof newLength !== 'number') throw TypeError("Length must be a number");
+    this._length = newLength;
+  }
 
-    get length() {
-        return this._length;
-    }
+  get length() {
+    return this._length;
+  }
 
-    set students(newStudents) {
-        this._students = newStudents;
-    }
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) throw TypeError("Students must be an array");
+    this._students = newStudents;
+  }
 
-    get students() {
-        return this._students;
-    }
+  get students() {
+    return this._students;
+  }
 }
