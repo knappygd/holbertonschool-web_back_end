35474a810d76d21c5f1f497a632eeaad30d37ad1
knapp <emiliodamasco04@gmail.com>
2023-09-14 09:44:58 -0700

    fixed lint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined)
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw Error('Class extending Building must override evacuationWarningMessage');
-    this._sqft = sqft
+    }
+    this._sqft = sqft;
   }
 
   set sqft(newSqft) {
