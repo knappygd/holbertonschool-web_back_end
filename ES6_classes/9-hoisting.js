c2c12b60922f375f67c9f6e11f089047af0ab943
knappygd <emiliodamasco04@gmail.com>
2023-09-15 12:54:02 -0700

    fixed self instead of this

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -30,11 +30,11 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
-    return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass._year} - ${this._holbertonClass._location}`;
   }
 }
 
