6f1a13d5022f8494a97bac1402bf9677b0289c3b
knapp <emiliodamasco04@gmail.com>
2023-10-02 10:46:13 -0700

    added 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,7 @@
 export default function handleResponseFromAPI(promise) {
+  const response = { status: 200, body: 'success' };
   return promise
-    .then(() => console.log({ status: 200, body: 'success' }))
-    .catch((error) => console.log(error))
+    .then(() => response)
+    .catch((error) => error)
     .finally(() => console.log('Got a response from the API'));
 }
