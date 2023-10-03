982059a2d3037089175e3a635a21255310c1b1c3
knapp <emiliodamasco04@gmail.com>
2023-10-03 06:43:23 -0700

    fixed lint

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((data) => {
     console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
-  }).catch(() => console.log("Signup system offline"));
+  }).catch(() => console.log('Signup system offline'));
 }
