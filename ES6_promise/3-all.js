6dc73e2b8c97c39ffca3ad6beac3a1fa791359b6
knapp <emiliodamasco04@gmail.com>
2023-10-03 06:29:55 -0700

    added 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import { createUser, uploadPhoto } from "./utils";
+
+export default function handleProfileSignup() {
+  return Promise.all([createUser(), uploadPhoto()]).then((data) => {
+    console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+  }).catch(() => console.log("Signup system offline"));
+}
