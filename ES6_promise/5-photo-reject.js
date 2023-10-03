cd6cf1e3a15f0b142137d807dcbd612566840068
knapp <emiliodamasco04@gmail.com>
2023-10-03 07:26:34 -0700

    removed console.log

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,3 +1,3 @@
 export default function uploadPhoto(filename) {
-  return Promise.reject(console.log(Error(`${filename} cannot be processed`)));
+    return Promise.reject(Error(`${filename} cannot be processed`));
 }
