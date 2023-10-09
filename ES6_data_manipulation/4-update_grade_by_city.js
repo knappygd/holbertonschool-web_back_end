7e2cc7f5870449b42bcd9e2c9c83a1b74c06f4ec
knapp <emiliodamasco04@gmail.com>
2023-10-09 08:57:30 -0700

    added 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,15 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    let arr = [];
-    arr = students.map((student) => {
-        if (student.location === city) {
-            let grade = 'N/A';
-            newGrades.forEach((dict) => {
-                if (student.id === dict.studentId)
-                    grade = dict.grade;
-            });
-            return { ...student, grade };
-        }
-        return null;
-    }).filter((result) => result !== null);
+  let arr = [];
+  arr = students.map((student) => {
+    if (student.location === city) {
+      let grade = 'N/A';
+      newGrades.forEach((dict) => {
+        if (student.id === dict.studentId) grade = dict.grade;
+      });
+      return { ...student, grade };
+    }
+    return null;
+  }).filter((result) => result !== null);
 
-    return arr;
+  return arr;
 }
