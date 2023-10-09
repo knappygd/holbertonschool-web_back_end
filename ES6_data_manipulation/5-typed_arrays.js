7c0dd0ad0f49d2f0ce76cb41454aa5892e8162c5
knapp <emiliodamasco04@gmail.com>
2023-10-09 11:08:06 -0700

    fixed lint

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
-    const arr = new Int8Array(length);
-    if (position > length) throw Error('Position outside range');
-    arr[position] = value;
-    const { buffer } = arr;
-    return new DataView(buffer, 0, length);
+  const arr = new Int8Array(length);
+  if (position > length) throw Error('Position outside range');
+  arr[position] = value;
+  const { buffer } = arr;
+  return new DataView(buffer, 0, length);
 }
