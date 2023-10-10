36d3e9519ac4be44ad76ab140d75809328461461
knapp <emiliodamasco04@gmail.com>
2023-10-10 06:48:13 -0700

    fixed non string input

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (startString === '' || typeof startString !== 'string') return '';
   let ret = '';
   for (const i of set) if (i && i.startsWith(startString)) ret += i.replace(startString, '-');
   return ret.substring(1);
